8f99e53b4174359cad7103ce171aa452917f57e9
oteca58 <oteca@protonmail.com>
2022-08-18 12:36:32 +0200

    added get by id api

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/productModels");
 
+// @desc Get All Products
+// @route GET /api/products
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
@@ -11,6 +13,24 @@ async function getProducts(req, res) {
   }
 }
 
+// @desc Get Single Products
+// @route GET /api/product/id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product not found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getProducts,
+  getProduct,
 };
